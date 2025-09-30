6294ad1697ecc1feeaa7915899d24427a7eb74fb
ntino67 <matteo.heidelberger@viacesi.fr>
2025-09-30 10:41:04 +0200

    Added Leaf Nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -26,3 +26,13 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("a", "it's a link", children, {"href": "http://BMC.com/"})
         node2 = HTMLNode("a", "it's a link", children2, {"href": "http://BMC.com/"})
         self.assertNotEqual(node, node2)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_p_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
